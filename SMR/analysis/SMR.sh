0d100e7ee811e4ddec3a73587d39604afdecf980
Yazhou Guo <guoyazhou@westlake.edu.cn>
2024-10-16 17:28:01 +0800

    SMR Portal SMR analysis script update

--- SMR/analysis/SMR.sh
+++ SMR/analysis/SMR.sh
@@ -13,6 +13,7 @@ OUTPUT=`yq .input.output "${CONFIG}"`
 mkdir -p ${OUTPUT}/SMR/detail
 mkdir -p ${OUTPUT}/SMR/summary
 mkdir -p ${OUTPUT}/SMR/smr
+mkdir -p ${OUTPUT}/SMR/SMR_Portal
 
 # ------------------------------------------------------------------------
 #  SMR analysis
@@ -20,21 +21,72 @@ mkdir -p ${OUTPUT}/SMR/smr
 
 SMR=`yq .software.smr "${CONFIG}"`
 REFERENCE_bld=`yq .reference.reference_bld "${CONFIG}"`
-QTL_list=`yq .smr.QTL_list "${CONFIG}"`
+QTL_list=`yq .magic.QTL_list "${CONFIG}"`
+
+
+maf=`yq .smr.maf "${CONFIG}"`
+peqtl_smr=`yq .smr.peqtl_smr "${CONFIG}"`
+peqtl_heidi=`yq .smr.peqtl_heidi "${CONFIG}"`
+batch_num=`yq .smr.batch_num "${CONFIG}"`
+smr_multi_index=`yq .smr.smr_multi_index "${CONFIG}"`
+
+
+default_maf=0.01
+default_peqtl_smr=5e-8
+default_peqtl_heidi=1.57e-3
+default_smr_multi_index=FALSE
+
+# maf=${maf:-$default_maf}
+# peqtl_smr=${peqtl_smr:-$default_peqtl_smr}
+# peqtl_heidi=${peqtl_heidi:-$default_peqtl_heidi}
+# 如果提取到的值为 "null" 或者为空，则使用默认值
+if [[ -z "$maf" || "$maf" == "null" ]]; then
+    maf=$default_maf
+fi
+
+if [[ -z "$peqtl_smr" || "$peqtl_smr" == "null" ]]; then
+    peqtl_smr=$default_peqtl_smr
+fi
+
+if [[ -z "$peqtl_heidi" || "$peqtl_heidi" == "null" ]]; then
+    peqtl_heidi=$default_peqtl_heidi
+fi
+
+if [[ -z "$smr_multi_index" || "$smr_multi_index" == "null" ]]; then
+    smr_multi_index=$default_smr_multi_index
+fi
+
+
+# **** Here we take qtl as a unit. In total 230 xQTL datasets.
+# 1-149: eQTL
+# 150-200: sQTL
+# 201-206: pQTL
+# 207-217: mQTL
+# 218-224: hQTL
+# 225-229: caQTL
 
 # ----
-qtl_i=${SLURM_ARRAY_TASK_ID}
+# old QTL list:
 # **** Here we take qtl as a unit.
+# QTL_list=`yq .smr.QTL_list "${CONFIG}"`
 # 1-78: eQTL
 # 79-129: sQTL
 # 130-131: mQTL
 # 132-136: pQTL
 
-qtl_name=`head -n ${qtl_i} ${QTL_list} | tail -n1 | awk -F "\t" '{print $1}'`
-qtl_data=`head -n ${qtl_i} ${QTL_list} | tail -n1 | awk -F "\t" '{print $2}'`
-qtl_chr=`head -n ${qtl_i} ${QTL_list} | tail -n1 | awk -F "\t" '{print $3}'`
+qtl_i=${SLURM_ARRAY_TASK_ID}
+
+qtl_name=`awk -F "\t" -v row=$qtl_i 'NR==row {print $1}' $QTL_list`
+qtl_data=`awk -F "\t" -v row=$qtl_i 'NR==row {print $2}' $QTL_list`
+qtl_chr=`awk -F "\t" -v row=$qtl_i 'NR==row {print $3}' $QTL_list`
 
 
+if [ -z "$qtl_name" ]; then
+    echo "no QTL in line $qtl_i, skip"
+    exit 0
+fi
+
+if [ ! -f "${OUTPUT}/SMR/summary/${trait_name}_${qtl_name}_chrALL.msmr" ]; then
 
 for i in $(seq 1 22); do
 
@@ -44,18 +96,63 @@ for i in $(seq 1 22); do
         QTL_data="${qtl_data}"
     fi
 
-    "${SMR}" --bld "${REFERENCE_bld}_chr${i}" \
-        --gwas-summary "${GWAS_DATA}" \
-        --beqtl-summary "${QTL_data}" \
-        --maf 0.01 \
-        --smr-multi \
-        --thread-num 4 \
-        --out "${OUTPUT}/SMR/detail/${trait_name}_${qtl_name}_chr${i}"
+    if [ "$smr_multi_index" = "TRUE" ]; then
+        
+        "${SMR}" --bld "${REFERENCE_bld}_chr${i}" \
+            --gwas-summary "${GWAS_DATA}" \
+            --beqtl-summary "${QTL_data}" \
+            --probe-chr ${i} \
+            --maf ${maf} \
+            --peqtl-smr ${peqtl_smr} \
+            --peqtl-heidi ${peqtl_heidi} \
+            --smr-multi \
+            --thread-num 4 \
+            --probe-chr ${i} \
+            --out "${OUTPUT}/SMR/detail/${trait_name}_${qtl_name}_chr${i}" &
+
+        pids+=($!)
+        if (( i % ${batch_num} == 0 )); then
+            for pid in "${pids[@]}"; do
+                wait $pid || [ $? -eq 99 ] && true || exit $?
+            done
+            pids=()
+        fi
+
+    else
+
+        "${SMR}" --bld "${REFERENCE_bld}_chr${i}" \
+            --gwas-summary "${GWAS_DATA}" \
+            --beqtl-summary "${QTL_data}" \
+            --probe-chr ${i} \
+            --maf ${maf} \
+            --peqtl-smr ${peqtl_smr} \
+            --peqtl-heidi ${peqtl_heidi} \
+            --thread-num 4 \
+            --probe-chr ${i} \
+            --out "${OUTPUT}/SMR/detail/${trait_name}_${qtl_name}_chr${i}" &
+
+        pids+=($!)
+        if (( i % ${batch_num} == 0 )); then
+            for pid in "${pids[@]}"; do
+                wait $pid || [ $? -eq 99 ] && true || exit $?
+            done
+            pids=()
+        fi
+
+    fi
 
 done
 
+# wait for all jobs to finish
+for pid in "${pids[@]}"; do
+    wait $pid || [ $? -eq 99 ] && true || exit $?
+done
 
-awk 'NR==1 || FNR>1' ${OUTPUT}/SMR/detail/${trait_name}_${qtl_name}_chr*.msmr > ${OUTPUT}/SMR/summary/${trait_name}_${qtl_name}_chrALL.msmr
 
-# rm ${OUTPUT}/SMR/detail/${trait_name}_${qtl_name}_chr*.
+# No matter smr or smr-multi, we will final output the "_chrALL.msmr" file 
+awk 'NR==1 || FNR>1' ${OUTPUT}/SMR/detail/${trait_name}_${qtl_name}_chr*smr > ${OUTPUT}/SMR/summary/${trait_name}_${qtl_name}_chrALL.msmr
+rm ${OUTPUT}/SMR/detail/${trait_name}_${qtl_name}_chr*
 
+else
+    echo "File already exists."
+fi
